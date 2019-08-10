869cdcd4313dd69cb6bc3a9eb14d76dd1a6087ec
digitalreign <muchenik@gmail.com>
2019-08-10 09:36:32 -0400

    Page 19 and 20, working with raw strings and encoding.

--- 1_helloworld.py
+++ 1_helloworld.py
@@ -16,3 +16,15 @@ Still here.''')
 
 # Page 18 Raw Strings
 print('C:\\Program FIles\\Vendor\\Application\\execute.exe')
+
+# Page 19 More Raw Strings
+print(r'C:\Program Files\Vendor\Application\HelloWorld.exe')
+# This one failed so I had to comment it out. 
+# print(r'C:\Program Files\Vendor\Application\')
+# Unicode, this shows up when run in python terminal but not in normal run.
+"\u00C7"
+
+# Page 20 Encoding
+"Hello, world!" .encode("ASCII")
+"Hello, world!" .encode("UTF-8")
+"Hello, world!" .encode("UTF-32")
