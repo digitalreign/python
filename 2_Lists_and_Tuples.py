1fb185d456483af263a79d8e50be3996778bd07a
digitalreign <muchenik@gmail.com>
2019-08-12 13:25:17 -0700

    20190812 Page 28 Slicing

--- 2_Lists_and_Tuples.py
+++ 2_Lists_and_Tuples.py
@@ -44,4 +44,16 @@ day_number = int(day)
 month_name = months[month_number-1]
 ordinal = day, endings[day_number-1]
 # This is not pretty but it works for now, need to get rid of the extra ( ) and ''
-print(month_name, ' ' , ordinal, ' ', year)
+print month_name, ordinal, year
+
+# 20190812 Page 28 Slicing
+tag = '<a href="http://www.python.org">Python web site</a>'
+print tag[9:30]
+print tag[32:-4]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print numbers [3:6]
+print numbers [0:1]
+print numbers [3:]
+print numbers [:3]
+print numbers [-3:]
+print numbers [:-3]
