b459ce64a939a9d1cb428681b9bc414ad5f3cc64
digitalreign <muchenik@gmail.com>
2019-08-22 15:31:06 -0700

    20190822 page 42-43 Tuples

---
+++ Chapter_2/2_Lists_and_Tuples_Tuples_Immutable_Sequences.py
@@ -0,0 +1,15 @@
+# 20190822 BeginningPython Chapter 2 
+
+# 20190822 page 42
+# Tuples: Immutable Sequences
+
+# A tuple is an immutable sequence. 
+# Should return 1212
+print(12 * (100+1))
+# 20190822 page 43
+# Should return ('1', '2', '1', '2')
+print(tuple('1212'))
+# Should return (12, 100, 1)
+print(tuple([12,100,1]))
+# Should return ('J', 'a', 'n', 'u', 'a', 'r', 'y')
+print(tuple('January'))
