3a6eeb886600e13b8d42c5972c8246c2b87c2423
digitalreign <digitalreign@users.noreply.github.com>
2019-08-27 22:49:27 -0400

    20190827 pg 50-52 Signs, Alignment

--- Chapter_3/3_Working_With_Strings_Basic_String_Operations.py
+++ Chapter_3/3_Working_With_Strings_Basic_String_Operations.py
@@ -46,3 +46,22 @@ print('Should print only the first 5 letters of January')
 print("{:.5}".format("January, February, March"))
 print('This is one googol.')
 print('{:,}'.format(10**100))
+# 20190827 pg 50 Signs, Alignment
+print('{0:<10}\n{0:^10}\n{0:>10}'.format('WIN BIG'))
+print("{:$^15}".format(" WIN BIG "))
+# String formatting
+# Print a formatted price list with a given width
+width = int(input('Please enter width that is greater than 10: '))
+price_width = 10
+item_width = width - price_width
+header_fmt = '{{:{}}}{{:>{}}}'.format(item_width, price_width)
+fmt = '{{:{}}}{{:>{}.2f}}'.format(item_width, price_width)
+print('=' * width)
+print(header_fmt.format('Item', 'Price'))
+print('-' * width)
+print(fmt.format('Apples', 0.4))
+print(fmt.format('Pears', 0.5))
+print(fmt.format('Cantaloupes', 1.92))
+print(fmt.format('Dried Apricots (16 oz.)', 8))
+print(fmt.format('Prunes (4 lbs.)', 12))
+print('=' * width)
