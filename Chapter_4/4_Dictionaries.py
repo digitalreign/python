f25a78397f7001f34c799ca3125df5e2a2d4f82e
digitalreign <muchenik@gmail.com>
2019-09-04 21:57:51 -0400

    20190904 page 59

---
+++ Chapter_4/4_Dictionaries.py
@@ -0,0 +1,9 @@
+# 20190904 BeginningPython Chapter 4
+# Dictionaries: When Indices Won’t Do
+
+# 20190904 page 59
+# Dictionary Uses
+
+months = ['January', 'February', 'March', 'April', 'May']
+numbers = ['31', '28', '31', '30', '31']
+print(numbers[months.index('February')])
