36f284aefee3fec0a30081d4ed120c09ba8e673c
digitalreign <digitalreign@users.noreply.github.com>
2019-09-05 12:45:05 -0400

    20190905 page 60-61 Basic Dictionary Operations

--- Chapter_4/4_Dictionaries.py
+++ Chapter_4/4_Dictionaries.py
@@ -7,3 +7,27 @@
 months = ['January', 'February', 'March', 'April', 'May']
 numbers = ['31', '28', '31', '30', '31']
 print(numbers[months.index('February')])
+
+# 20190905 page 60
+# Creating and Using Dictionaries
+
+calender = [('month', 'January'), ('days', 31)]
+cal = dict(calender)
+print(cal)
+
+# Basic Dictionary Operations page 61
+monthly = {}
+monthly[1] = 'January'
+monthly[2] = 'February'
+monthly[3] = 'March'
+monthly[4] = 'April'
+monthly[5] = 'May'
+monthly[6] = 'June'
+monthly[7] = 'July'
+monthly[8] = 'August'
+monthly[9] = 'September'
+monthly[10] = 'October'
+monthly[11] = 'November'
+monthly[12] = 'December'
+print('The Monthly Dictionary')
+print(monthly)
