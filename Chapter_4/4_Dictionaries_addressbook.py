db1a1bcdafa2325e2363ab981750543e30548ec5
digitalreign <digitalreign@users.noreply.github.com>
2019-09-12 16:45:50 -0400

    20190912 get page 66

--- Chapter_4/4_Dictionaries_addressbook.py
+++ Chapter_4/4_Dictionaries_addressbook.py
@@ -66,3 +66,24 @@ print(New_Seasons)
 week = {}
 print('Should return None')
 print(week.get('day'))
+
+# 20190912 get page 66
+
+# A simple database using get()
+# Insert database (people) from Listing 4-1 here.
+labels = {
+'phone': 'phone number',
+'addr': 'address'
+}
+name = input('Name: ')
+# Are we looking for a phone number or an address?
+request = input('Phone number (p) or address (a)? ')
+# Use the correct key:
+key = request # In case the request is neither 'p' nor 'a'
+if request == 'p': key = 'phone'
+if request == 'a': key = 'addr'
+# Use get to provide default values:
+person = people.get(name, {})
+label = labels.get(key, key)
+result = person.get(key, 'not available')
+print("{}'s {} is {}.".format(name, label, result))
