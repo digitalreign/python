a99846ce02551498bfcad85227cb2b5cdcc86067
digitalreign <digitalreign@users.noreply.github.com>
2019-09-25 12:16:11 -0400

    20190925 page 71 Printing Multiple Arguments

---
+++ Chapter_5/5_Conditionals_Loops.py
@@ -0,0 +1,11 @@
+# 20190925 BeginningPython Chapter 5
+# Conditional, Loops, and Some Other Statements
+
+# 20190925 page 71
+# Printing Multiple Arguments
+
+print('Month:', 'January')
+month = 'January'
+year = '2019'
+day = '01'
+print(day, month, year)
