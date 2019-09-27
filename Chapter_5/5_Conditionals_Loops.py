62faf63bea9f44df9f1d7b06113a65c2033b607b
digitalreign <digitalreign@users.noreply.github.com>
2019-09-27 00:37:24 -0400

    20190927 page 71-7 more printing arguements and importing

--- Chapter_5/5_Conditionals_Loops.py
+++ Chapter_5/5_Conditionals_Loops.py
@@ -9,3 +9,14 @@ month = 'January'
 year = '2019'
 day = '01'
 print(day, month, year)
+# 20190927 page 71 more printing arguements
+print("Adding comma after the month")
+print(day, month + ',', year)
+print("Adding seperator instead of space:")
+print(day, month, year, sep="_")
+
+# Importing Something as Something Else
+import math as notmath
+print(notmath.sqrt(4))
+from math import sqrt as notsqrt
+print(notsqrt(9))
