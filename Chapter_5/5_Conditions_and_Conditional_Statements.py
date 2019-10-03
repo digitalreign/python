c3593b0b13a61824f142c60840b6d0f86cf03791
digitalreign <muchenik@gmail.com>
2019-10-03 14:32:10 -0400

    20191003 page 78 if and else clauses

--- Chapter_5/5_Conditions_and_Conditional_Statements.py
+++ Chapter_5/5_Conditions_and_Conditional_Statements.py
@@ -3,8 +3,13 @@
 
 # 20191002 page 76-77 Conditions and Conditional Statements
 
-#True and False are just 1 and 0.
+# True and False are just 1 and 0.
 print('Should return 11:')
 print(True + 10)
 print('Should return True:')
 print(bool('Something here'))
+
+# 20191003 page 78 if and else clauses
+name = input('What is your name? ')
+if name.endswith('Gumby'):
+    print('Hello, Mr. Gumby')
