6d0fe94a946ac5ce5b2d9c7ed52f1e6ac2427b12
digitalreign <digitalreign@users.noreply.github.com>
2019-10-26 18:03:43 -0400

    20191026 page 90 Breaking Out of Loops

---
+++ Chapter_5/5_Conditions_Loops_Again.py
@@ -0,0 +1,49 @@
+# 20191002 BeginningPython Chapter 5
+# Conditional, Loops, and Some Other Statements
+
+# Loops
+# 20191018 page 85 while Loops
+x = 1
+while x <= 100:
+    print(x)
+    x += 1
+# 20191021 page 85-86 while Loops
+name = ''
+while not name:
+    name = input('Please enter your name: ')
+print('Hello, {}!'.format(name))
+words = [name, 'is', 'an', 'ex', 'parrot']
+for word in words:
+    print(word)
+
+# 20191022 page 87 Iterating Over Dictionaries
+d = {'x': 1, 'y': 2, 'z': 3}
+for key in d:
+    print(key, 'corresponds to', d[key])
+
+# 20191024 page 87 Parallel Iteration
+order = [1, 2, 3, 4, 5]
+for i in range (len(words)):
+    print (words[i], 'is the', order[i], 'word')
+print(list(zip(words, order)))
+for words, order in zip(words, order):
+    print(words, 'is', order, 'years old')
+
+# 20191025 page 89 Reversed and Sorted Iteration
+print(sorted([4, 3, 6, 8, 3]))
+print(sorted('Hello, world!'))
+print(list(reversed('Hello, world!')))
+print(''.join(reversed('Hello, world!')))
+
+# 20191026 page 90 Breaking Out of Loops
+from math import sqrt
+for n in range(99, 0, -1):
+    root = sqrt(n)
+    if root == int(root):
+        print(n)
+        break
+for n in range(2, 100, 1):
+    root = sqrt(n)
+    if root == int(root):
+        print(n)
+        break
